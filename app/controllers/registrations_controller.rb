ce7a26dc96b9ca955840873c82aae039ee09c69f
Tim Jedrek <tim@timjedrek.com>
2022-04-15 05:10:25 -0500

    fix syntax errors and fix tweeet controller by except for authentication

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,4 @@
-class RegistrationsController < devise::RegistrationsController
+class RegistrationsController < Devise::RegistrationsController
 
   private
   
@@ -9,3 +9,5 @@ class RegistrationsController < devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
   end
+
+end
